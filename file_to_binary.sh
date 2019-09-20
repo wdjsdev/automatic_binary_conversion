0f07968b270bffd332eb675a47722f48a7dc9e8f
william <will.dowling@boombah.com>
2019-09-20 10:41:45 -0500

    removed function definitions from shell scripts

--- file_to_binary.sh
+++ file_to_binary.sh
@@ -1,49 +1,47 @@
 #function for converting a single file to binary instead
 #of a whole project (like the evalBin function)
-fileToBinary()
-{	
 
-	if [[ -z "$1" ]];then
-		echo "please call out a file for conversion"
-		return
-	fi
+if [[ -z "$1" ]];then
+	echo "please call out a file for conversion"
+	return
+fi
 
-	#jsx to jsxbin compiler script
-	compiler=~/automatic_binary_conversion/jsxbin_compiler.jsx
+#jsx to jsxbin compiler script
+compiler=~/automatic_binary_conversion/jsxbin_compiler.jsx
 
-	projectFolder=${PWD} &&
-	mainScript=$projectFolder/$1 &&
+projectFolder=${PWD} &&
+mainScript=$projectFolder/$1 &&
 
-	#make sure there's a convert_to_binary folder
-	if [ ! -d ~/automatic_binary_conversion/convert_to_binary ]; then
-		mkdir ~/automatic_binary_conversion/convert_to_binary
-		echo "made a convert_to_binary folder"
-	fi
-	convertedFiles=~/automatic_binary_conversion/binary_converted &&
+#make sure there's a convert_to_binary folder
+if [ ! -d ~/automatic_binary_conversion/convert_to_binary ]; then
+	mkdir ~/automatic_binary_conversion/convert_to_binary
+	echo "made a convert_to_binary folder"
+fi
+convertedFiles=~/automatic_binary_conversion/binary_converted &&
 
-	#make sure there's a binary_converted folder
-	if [ ! -d ~/automatic_binary_conversion/binary_converted ]; then
-		mkdir ~/automatic_binary_conversion/binary_converted
-		echo "made a binary_converted folder"
-	fi
-	filesToConvert=~/automatic_binary_conversion/convert_to_binary
+#make sure there's a binary_converted folder
+if [ ! -d ~/automatic_binary_conversion/binary_converted ]; then
+	mkdir ~/automatic_binary_conversion/binary_converted
+	echo "made a binary_converted folder"
+fi
+filesToConvert=~/automatic_binary_conversion/convert_to_binary
 
-	cp $mainScript $filesToConvert &&
-	echo "copied $mainScript"
+cp $mainScript $filesToConvert &&
+echo "copied $mainScript"
 
-	echo "the following files have been copied to filesToConvert"
-	ls $filesToConvert
+echo "the following files have been copied to filesToConvert"
+ls $filesToConvert
 
-	osascript -e 'quit app "ExtendScript Toolkit"'
+osascript -e 'quit app "ExtendScript Toolkit"'
 
-	#it's not an error if i don't get a message about it.
-	/Volumes/Macintosh\ HD/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/MacOs/ExtendScript\ Toolkit -cmd $compiler >/dev/null 2>&1 &&
-	
-	#noisy version..
-	# /Volumes/Macintosh\ HD/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/MacOs/ExtendScript\ Toolkit -cmd $compiler &&
+#it's not an error if i don't get a message about it.
+/Volumes/Macintosh\ HD/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/MacOs/ExtendScript\ Toolkit -cmd $compiler >/dev/null 2>&1 &&
 
-	mv $convertedFiles/*.jsx $projectFolder &&
+#noisy version..
+# /Volumes/Macintosh\ HD/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/MacOs/ExtendScript\ Toolkit -cmd $compiler &&
 
-	rm $filesToConvert/* &&
-	echo "Successfully created binary files."
-}
+mv $convertedFiles/*.jsx $projectFolder &&
+
+rm $filesToConvert/* &&
+echo "Successfully created binary files."
+
--- project_to_binary.sh
+++ project_to_binary.sh
@@ -1,50 +1,48 @@
-projectToBinary()
-{	
-	#jsx to jsxbin compiler script
-	compiler="~/automatic_binary_conversion/jsxbin_compiler.jsx"
 
-	projectFolder=${PWD} &&
-	mainScript=$projectFolder/*Dev.js* &&
-	components=$projectFolder/components &&
+#jsx to jsxbin compiler script
+compiler="~/automatic_binary_conversion/jsxbin_compiler.jsx"
 
-	#make sure that there's a bin_comp folder in 
-	#the project folder
-	if [ ! -d "bin_comp" ]; then
-		mkdir $projectFolder/bin_comp
-		echo "made a bin_comp folder"
-	fi
-	binComp=$projectFolder/bin_comp
+projectFolder=${PWD} &&
+mainScript=$projectFolder/*Dev.js* &&
+components=$projectFolder/components &&
 
-	#make sure there's a convert_to_binary folder
-	if [ ! -d ~/automatic_binary_conversion/convert_to_binary ]; then
-		mkdir ~/automatic_binary_conversion/convert_to_binary
-		echo "made a convert_to_binary folder"
-	fi
-	convertedFiles=~/automatic_binary_conversion/binary_converted &&
+#make sure that there's a bin_comp folder in 
+#the project folder
+if [ ! -d "bin_comp" ]; then
+	mkdir $projectFolder/bin_comp
+	echo "made a bin_comp folder"
+fi
+binComp=$projectFolder/bin_comp
 
-	#make sure there's a binary_converted folder
-	if [ ! -d ~/automatic_binary_conversion/binary_converted ]; then
-		mkdir ~/automatic_binary_conversion/binary_converted
-		echo "made a binary_converted folder"
-	fi
-	filesToConvert=~/automatic_binary_conversion/convert_to_binary
+#make sure there's a convert_to_binary folder
+if [ ! -d ~/automatic_binary_conversion/convert_to_binary ]; then
+	mkdir ~/automatic_binary_conversion/convert_to_binary
+	echo "made a convert_to_binary folder"
+fi
+convertedFiles=~/automatic_binary_conversion/binary_converted &&
 
-	cp $mainScript $filesToConvert &&
-	echo "copied mainScript"
+#make sure there's a binary_converted folder
+if [ ! -d ~/automatic_binary_conversion/binary_converted ]; then
+	mkdir ~/automatic_binary_conversion/binary_converted
+	echo "made a binary_converted folder"
+fi
+filesToConvert=~/automatic_binary_conversion/convert_to_binary
 
-	cp $components/*.js $filesToConvert &&
-	echo "copied components"
+cp $mainScript $filesToConvert &&
+echo "copied mainScript"
 
-	osascript -e 'quit app "ExtendScript Toolkit"'
+cp $components/*.js $filesToConvert &&
+echo "copied components"
 
-	/Volumes/Macintosh\ HD/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/MacOs/ExtendScript\ Toolkit -cmd $compiler >/dev/null 2>&1 &&
+osascript -e 'quit app "ExtendScript Toolkit"'
 
-	mv $convertedFiles/*.jsx $projectFolder &&
-	echo "Moved $(basename $projectFolder/*.jsx) binary file to project folder."
+/Volumes/Macintosh\ HD/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/MacOs/ExtendScript\ Toolkit -cmd $compiler >/dev/null 2>&1 &&
 
-	mv $convertedFiles/*.jsxbin $binComp &&
-	echo "Moved binary components to bin_comp"
+mv $convertedFiles/*.jsx $projectFolder &&
+echo "Moved $(basename $projectFolder/*.jsx) binary file to project folder."
 
-	rm $filesToConvert/* &&
-	echo "Successfully created binary files."
-}
+mv $convertedFiles/*.jsxbin $binComp &&
+echo "Moved binary components to bin_comp"
+
+rm $filesToConvert/* &&
+echo "Successfully created binary files."
